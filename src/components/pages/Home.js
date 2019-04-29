de2f82d1b0e3cfef565c533c9c1ec88a781229cd
Akshay Sonawane <akshay@MacBook-Pro-2.local>
2019-04-29 23:18:01 +0530

    Moved App.scss into scss folder, and fixed responsive design on collections and home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -11,19 +11,24 @@ import Spinner from '../../assets/images/oval.svg';
 class Home extends Component {
     constructor(props) {
         super(props)
+        this._isMounted = false;
         this.state = {
             isLoading: true,
             images: []
         }
     }
     componentDidMount() {
+        this._isMounted = true;
         this.fetchLatestPhotos();
+    }
+    componentWillUnmount() {
+        this._isMounted = false;
     }
     fetchLatestPhotos = () => {
         unsplash.photos.listPhotos(1, 15, "latest")
         .then(toJson)
         .then(json => {
-            this.setState ({
+            this._isMounted && this.setState ({
                 images: json,
                 isLoading: false
             })
@@ -36,7 +41,7 @@ class Home extends Component {
                     <h1 className="text-3xl">ReactSplash is built on the top of Unsplash APIs</h1>
                 {!this.state.isLoading ?
                     <Masonry
-                    className={'images--container p-0 -mx-4'} // default ''
+                    className={'images-container p-0 -mx-4'} // default ''
                     options={masonryOptions} // default {}
                     disableImagesLoaded={false} // default false
                     updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom' ;
-import './App.scss';
+import './assets/scss/App.scss';
 import App from './App';
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react';
